7c35d9a35156955d6f467e2098f48a2e59d83704
Elma Rahman <elmarahman635@gmail.com>
2019-05-19 17:16:49 -0400

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,28 @@
 require_relative '../lib/author.rb'
 
 class Post
-  attr_accessor :author
+  attr_accessor :author, :title
 
 @@all = []
 
-def initialize
-  @author = author
+def initialize(title)
+  @title = title
+
   @@all << self
 
+end
+
+
+def self.all
+  @@all
+end
+
+def author_name
+  if self.author == nil
+    nil
+  else
+    author.name
+  end
 end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,35 +1,38 @@
 require_relative '../lib/artist.rb'
 
 class Song
-  attr_accessor :artist, :name 
+  attr_accessor :artist, :name, :song #this will hold all responsibility so you call artist class here to reference to it
 
-@@all = [] #always do @@all will always be class variable & initialze as an empty array & pass every instance will go through initialize -- bc the only way you'll make a new var is .new.
+@@all = [] # @@all will always be class variable & initialze as an empty array &
+#pass every instance will go through initialize -- bc the only way you'll make a new var is .new.
 
 def initialize(name)
   @name = name
-
-  @@all << self
-end
-
-def songs
-
-
+  @@all << self #generel rule
 end
 
 def self.all #reveals everything in @@all-- you need this to be able to call on that class containing the array.
   @@all
 end
 
-
-# def add_song_by_name(name)
-#   self.new
-# end
-
-# def some_song.artist_name
+#DONT NEED THIS:
+# def self.artist
+#     Songs.all.select do |song|
+#       song.name == artist.name
+#         Artist.name
 #   #set an iterator to go through array of songs
 #   #to organize it so that it spits out artist of given song
-#   @artist.name
-# end
+#     end
+#   end
+
+def artist_name
+  if self.artist == nil  #findining artist of song
+    nil
+  else
+    artist.name
+    end
+  end
+
 
 
 end
