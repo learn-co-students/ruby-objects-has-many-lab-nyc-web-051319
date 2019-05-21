584b8efb22ff60639f220788e3bae0d7f8a8a3ef
Aniket <aniketupatel1@gmail.com>
2019-05-21 06:16:48 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+
+  attr_accessor :name, :posts
+
+  @@counter = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@counter += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@counter += 1
+  end
+
+  def self.post_count
+    @@counter
+  end
+
+end
